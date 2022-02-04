9aa854c53e0b7e28cb6813990a771680ff521929
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2022-02-05 00:58:20 +0300

    feat: return versions via simple command

--- gui/src-tauri/src/main.rs
+++ gui/src-tauri/src/main.rs
@@ -3,8 +3,14 @@
     windows_subsystem = "windows"
 )]
 
+#[tauri::command]
+async fn get_versions() -> Result<Vec<String>, ()> {
+    Ok(vec![String::from("v0.3"), String::from("v0.4")])
+}
+
 fn main() {
     tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![get_versions])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
