9b9d3b7771479b387deb5894425d70cc70edf3a2
bufgix <ooruc471@yandex.com>
2022-02-04 21:20:09 +0300

    refactor: remove mf style directory

--- gui/src/App.svelte
+++ gui/src/App.svelte
@@ -4,28 +4,20 @@
 
 <main>
   <div class="container">
-    <div class="changelog">
-      <h1>contetn</h1>
-    </div>
+    <div class="changelog" />
     <Sidebar />
   </div>
 </main>
 
 <style lang="scss">
   @import url("https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap");
-  @import "styles/app.scss";
 
   .container {
     display: flex;
     flex-direction: row;
 
     .changelog {
-      flex: 7;
-    }
-    .sidebar {
-      flex: 3;
-      background-color: $color-white;
-      height: 100vh;
+      flex: 5;
     }
   }
 </style>
--- gui/src/components/Sidebar.svelte
+++ gui/src/components/Sidebar.svelte
@@ -1,13 +1,20 @@
 <section class="sidebar">
+  <img alt="Fish Fight logo" src="images/logo.png" class="logo mt-4" />
   <p>Sidebar</p>
 </section>
 
 <style lang="scss">
-  @import "../styles/config/variables.scss";
-
   .sidebar {
+    display: flex;
+    flex-direction: column;
     flex: 3;
-    background-color: $color-white;
+    background-color: white;
     height: 100vh;
+
+    .logo {
+      align-self: center;
+      width: 70%;
+      margin-top: 10px;
+    }
   }
 </style>
